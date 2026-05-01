98e1d6acda86b787a67a6d149c20af385af8daea
Zohaib <tariqzohaib37@gmail.com>
2026-05-01 05:04:09 -0700

    made banner.java for all attributes getter setter

---
+++ app/src/main/java/com/example/ezshopapp/Banner.java
@@ -0,0 +1,22 @@
+package com.example.ezshopapp;
+
+public class Banner {
+    private String title;
+    private String subtitle;
+    private String discountText;
+    private String imageUrl;
+
+    public Banner() {}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getSubtitle() { return subtitle; }
+    public void setSubtitle(String subtitle) { this.subtitle = subtitle; }
+
+    public String getDiscountText() { return discountText; }
+    public void setDiscountText(String discountText) { this.discountText = discountText; }
+
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+}
