951ea0acf331ee3248e9cdab896eb7a468fcc500
Zohaib Tariq <tariqzohaib37@gmail.com>
2026-05-02 15:42:05 -0700

    made order adapter to display list of past few orders by user

---
+++ app/src/main/java/com/example/ezshopapp/OrderAdapter.java
@@ -0,0 +1,74 @@
+package com.example.ezshopapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
+
+    private List<Order> orderList;
+
+    public OrderAdapter(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    @NonNull
+    @Override
+    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order, parent, false);
+        return new OrderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
+        Order order = orderList.get(position);
+
+        if (order.getTimestamp() != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault());
+            holder.tvOrderDate.setText("Date: " + sdf.format(order.getTimestamp()));
+        } else {
+            holder.tvOrderDate.setText("Date: N/A");
+        }
+
+        holder.tvOrderStatus.setText(order.getStatus());
+        
+        // Build items string
+        StringBuilder itemsBuilder = new StringBuilder();
+        if (order.getItems() != null) {
+            for (int i = 0; i < order.getItems().size(); i++) {
+                CartItem item = order.getItems().get(i);
+                itemsBuilder.append(item.getProductName()).append(" x").append(item.getQuantity());
+                if (i < order.getItems().size() - 1) {
+                    itemsBuilder.append(", ");
+                }
+            }
+        }
+        holder.tvOrderItems.setText(itemsBuilder.toString());
+        holder.tvOrderTotal.setText(String.format(Locale.getDefault(), "$ %.2f", order.getTotalAmount()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList != null ? orderList.size() : 0;
+    }
+
+    public static class OrderViewHolder extends RecyclerView.ViewHolder {
+        TextView tvOrderDate, tvOrderStatus, tvOrderItems, tvOrderTotal;
+
+        public OrderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvOrderDate = itemView.findViewById(R.id.tvOrderDate);
+            tvOrderStatus = itemView.findViewById(R.id.tvOrderStatus);
+            tvOrderItems = itemView.findViewById(R.id.tvOrderItems);
+            tvOrderTotal = itemView.findViewById(R.id.tvOrderTotal);
+        }
+    }
+}
